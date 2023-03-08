4d32746a2f4cd19acb9086df04afd7483463306c
tracer-nju <tracer@nju.edu.cn>
2023-03-08 20:58:09 +0800

    Linux birdium-ms7c94 5.15.94-1-MANJARO #1 SMP PREEMPT Tue Feb 14 19:45:58 UTC 2023 x86_64 GNU/Linux  20:58:09 up 53 min,  3 users,  load average: 0.54, 0.81, 0.74

--- .shadow/pstree/pstree.c
+++ .shadow/pstree/pstree.c
@@ -1,11 +1,113 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <assert.h>
+#include <dirent.h>
+#include <getopt.h>
+
+#define MAX_PROCS  114514
+#define NAME_LEN  256
+
+struct Proc{
+  int pid, ppid;
+  char name[NAME_LEN];
+} procs[MAX_PROCS];
+
+
+int cnt;
+int show_pids = 0, numeric_sort = 0;
+
+void print_version() {
+  fprintf(stderr, "pstree lite 114.514\n");
+  fprintf(stderr, "Copyright (C) 1919-810 Birdium\n\n");
+  fprintf(stderr, "PSmisc comes with ABSOLUTELY NO WARRANTY.\nThis is free software, and you are welcome to redistribute it under\nthe terms of the GNU General Public License.\nFor more information about these matters, see the files named COPYING.\n");
+}
+
+void get_procs(){
+  DIR *dp;
+  struct dirent *entry;
+  dp = opendir("/proc");
+  if (dp){
+    while((entry = readdir(dp)) != NULL) {
+      if (atoll(entry->d_name) > 0) {
+        // printf("PID:%s\t", entry->d_name);
+        char path[280];
+        pid_t pid = 0, ppid; char proc_name[256];
+        sprintf(path, "/proc/%s/status", entry->d_name);
+        FILE *fp = fopen(path, "r");
+        if (fp == NULL) {
+          assert(0);
+        }
+        fscanf(fp, "Name:\t%s", proc_name);
+        while (fscanf(fp, "Pid:\t%d", &pid) != 1){
+          fgets(path, 280, fp);
+        }
+        while (fscanf(fp, "PPid:\t%d", &ppid) != 1){
+          fgets(path, 280, fp);
+        }
+        strcpy(procs[cnt].name, proc_name);
+        procs[cnt].pid = pid;
+        procs[cnt].ppid = ppid;
+        cnt++;
+        // printf("%s\n", proc_name);
+        fclose(fp);
+      }
+    }
+
+    closedir(dp);
+    return;
+  }
+  assert(0);
+}
+
+void dfs(int x, int dep){
+  for(int i = 0; i < dep; i++) {
+    printf("\t");
+  }
+  printf("%s", procs[x].name);
+  if (show_pids) printf("(%d)", procs[x].pid);
+  printf("\n");
+  for(int i = 0; i < cnt; i++) {
+    if (procs[i].ppid == procs[x].pid) {
+      dfs(i, dep + 1);
+    }
+  }
+}
+
+void print_tree(){
+  int st = 0;
+  for (int i = 0; i < cnt; i++){
+    if (procs[i].ppid == 0){
+      st = i; break;
+    } 
+  }
+  dfs(st, 0);
+}
+
+void parse_args(int argc, char *argv[]) {
+  int o;
+  static struct option table[] = {
+    {"show-pids", no_argument, 0, 'p'},
+    {"numeric-sort", no_argument, 0, 'n'},
+    {"version", no_argument, 0, 'V'}
+  };
+  while ( (o = getopt_long(argc, argv, "", table, NULL)) != -1 ) {
+    switch (o) {
+      case 'p': show_pids = 1; break;
+      case 'n': numeric_sort = 1; break;
+      case 'V': print_version(); exit(0);
+      default: break;
+    }
+  }
+}
 
 int main(int argc, char *argv[]) {
   for (int i = 0; i < argc; i++) {
     assert(argv[i]);
-    printf("argv[%d] = %s\n", i, argv[i]);
+    // printf("argv[%d] = %s\n", i, argv[i]);
   }
   assert(!argv[argc]);
+  parse_args(argc, argv);
+  get_procs();
+  print_tree();
   return 0;
 }

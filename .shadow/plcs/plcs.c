6f9e3b97600865a64b63ca79f075e3a6162f728c
tracer-nju <tracer@nju.edu.cn>
2023-03-14 14:47:06 +0800

    Linux birdium-GL65-Leopard-10SFSK 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:47:06 up 24 min,  1 user,  load average: 2.72, 2.36, 1.44

--- .shadow/plcs/plcs.c
+++ .shadow/plcs/plcs.c
@@ -10,26 +10,83 @@ char A[MAXN + 1], B[MAXN + 1];
 int dp[MAXN][MAXN];
 int result;
 
+int xchg(int volatile *ptr, int newval) {
+  int result;
+  asm volatile(
+    "lock xchgl %0, %1"
+    : "+m"(*ptr), "=a"(result)
+    : "1"(newval)
+    : "memory"
+  );
+  return result;
+}
+
+int locked = 0;
+
+void lock() {
+  while (xchg(&locked, 1)) ;
+}
+
+void unlock() {
+  xchg(&locked, 0);
+}
+
+int commit_cnt = 0;
+int flag[4] = {1, 1, 1, 1};
+
 #define DP(x, y) (((x) >= 0 && (y) >= 0) ? dp[x][y] : 0)
 #define MAX(x, y) (((x) > (y)) ? (x) : (y))
+#define MIN(x, y) (((x) < (y)) ? (x) : (y))
 #define MAX3(x, y, z) MAX(MAX(x, y), z)
 
+// Always try to make DP code more readable
+inline void calc(int i, int j) {
+  int skip_a = DP(i - 1, j);
+  int skip_b = DP(i, j - 1);
+  int take_both = DP(i - 1, j - 1) + (A[i] == B[j]);
+  dp[i][j] = MAX3(skip_a, skip_b, take_both);
+}
+
 void Tworker(int id) {
-  if (id != 1) {
+  if (id > T) {
     // This is a serial implementation
     // Only one worker needs to be activated
     return;
   }
 
+#ifdef SINGLE
   for (int i = 0; i < N; i++) {
     for (int j = 0; j < M; j++) {
-      // Always try to make DP code more readable
-      int skip_a = DP(i - 1, j);
-      int skip_b = DP(i, j - 1);
-      int take_both = DP(i - 1, j - 1) + (A[i] == B[j]);
-      dp[i][j] = MAX3(skip_a, skip_b, take_both);
+       calc(i, j);
     }
   }
+#else
+  for (int k = 0; k < M + N - 1; k++) {
+    lock();
+    if (commit_cnt == T) {
+      commit_cnt = 0;
+      for (int i = 0; i < T; i++) {
+        flag[i] = 1;
+      }
+    }
+    unlock();
+    while (1) {
+      lock();
+      if (flag[id - 1]) {
+        flag[id - 1] = 0;
+        break;
+      }
+      unlock();
+    }
+    unlock();
+    for (int j = id - 1 + MAX(0, k - N + 1); j <= MIN(k, M - 1); j += T) { 
+      calc(k - j, j);
+    }
+    lock();
+    commit_cnt++;
+    unlock();
+  }
+#endif
 
   result = dp[N - 1][M - 1];
 }

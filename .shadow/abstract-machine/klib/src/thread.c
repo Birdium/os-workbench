c9d0e530dacc346d6e3dea0447727dcb8c15b883
tracer-nju <tracer@nju.edu.cn>
2023-03-16 04:56:33 +0800

    Linux birdium-ms7c94 5.15.94-1-MANJARO #1 SMP PREEMPT Tue Feb 14 19:45:58 UTC 2023 x86_64 GNU/Linux  04:56:33 up  3:32,  4 users,  load average: 1.19, 0.88, 0.62

--- .shadow/abstract-machine/klib/src/thread.c
+++ .shadow/abstract-machine/klib/src/thread.c
@@ -1,14 +1,14 @@
-// #include <klib.h>
+#include <klib.h>
 
-// void spin_lock(spinlock_t *lk) {
-//   while (1) {
-//     intptr_t value = atomic_xchg(lk, 1);
-//     if (value == 0) {
-//       break;
-//     }
-//   }
-// }
+void spin_lock(spinlock_t *lk) {
+  while (1) {
+    intptr_t value = atomic_xchg(lk, 1);
+    if (value == 0) {
+      break;
+    }
+  }
+}
 
-// void spin_unlock(spinlock_t *lk) {
-//   atomic_xchg(lk, 0);
-// }
+void spin_unlock(spinlock_t *lk) {
+  atomic_xchg(lk, 0);
+}

d5f478a590b519d629de22d436fb6d85cd3b6ef7
tracer-nju <tracer@nju.edu.cn>
2023-03-21 13:54:51 +0800

    Linux birdium-GL65-Leopard-10SFSK 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:54:51 up 35 min,  1 user,  load average: 0.96, 1.14, 0.93

--- .shadow/plcs/plcs.c
+++ .shadow/plcs/plcs.c
@@ -8,7 +8,7 @@
 #define MAXN 10000
 int T, N, M;
 char A[MAXN + 1], B[MAXN + 1];
-int dp[MAXN][MAXN];
+int dp[MAXN * 2][MAXN];
 int result;
 
 mutex_t lock = MUTEX_INIT();
@@ -22,19 +22,19 @@ int commit_cnt = 0;
 #define MAX3(x, y, z) MAX(MAX(x, y), z)
 
 // Always try to make DP code more readable
-inline void calc(int i, int j) {
+void calc(int i, int j) {
   int skip_a = DP(i - 1, j);
   int skip_b = DP(i, j - 1);
   int take_both = DP(i - 1, j - 1) + (A[i] == B[j]);
   dp[i][j] = MAX3(skip_a, skip_b, take_both);
 }
 
-// inline void calc_t(int i, int j) {
-//   int skip_a = DP(i - 1, j);
-//   int skip_b = DP(i - 1, j - 1);
-//   int take_both = DP(i - 2, j - 1) + (A[i - j] == B[j]);
-//   dp[i][j] = MAX3(skip_a, skip_b, take_both);
-// }
+void calc_t(int i, int j) {
+  int skip_a = DP(i - 1, j);
+  int skip_b = DP(i - 1, j - 1);
+  int take_both = DP(i - 2, j - 1) + (A[i - j] == B[j]);
+  dp[i][j] = MAX3(skip_a, skip_b, take_both);
+}
 
 void Tworker(int id) {
   if (id > T) {
@@ -55,7 +55,7 @@ void Tworker(int id) {
     int L = MAX(0, k - N + 1), R = MIN(k + 1, M);
     int l = L + (R - L) / T * (id - 1), r = (id != T) ? (L + (R - L) / T * id) : R;
     for (int j = l; j < r; j++) { 
-      calc(k - j, j);
+      calc_t(k, j);
     }
     // for (int j = L + id - 1; j < R; j += T) {
     //   calc(k - j, j);
@@ -73,7 +73,7 @@ void Tworker(int id) {
     mutex_unlock(&lock);
   }
   // }
-  result = dp[N - 1][M - 1];
+  result = dp[N + M - 2][M - 1];
 #endif
 
 }

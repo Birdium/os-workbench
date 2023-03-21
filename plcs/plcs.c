ca122d67c599fb787629b3b30ed2646f3332e7c7
Birdium <bibishu2001@163.com>
2023-03-21 15:17:13 +0800

    check L = 1

--- plcs/plcs.c
+++ plcs/plcs.c
@@ -12,9 +12,9 @@ int dp[MAXN * 2][MAXN];
 int result;
 
 mutex_t lock = MUTEX_INIT();
+cond_t cv = COND_INIT();
 
 int commit_cnt = 0;
-int flag[4] = {1, 1, 1, 1};
 
 #define DP(x, y) (((x) >= 0 && (y) >= 0) ? dp[x][y] : 0)
 #define MAX(x, y) (((x) > (y)) ? (x) : (y))
@@ -43,62 +43,39 @@ void Tworker(int id) {
     return;
   }
 
-#ifdef SINGLE
+if (T == 1) {
   for (int i = 0; i < N; i++) {
     for (int j = 0; j < M; j++) {
        calc(i, j);
     }
   }
   result = dp[N - 1][M - 1];
-  // for (int k = 0; k < M + N - 1; k++) {
-  //   int L = MAX(0, k - N + 1), R = MIN(k + 1, M);
-  //   for (int j = L; j < R; j++) { 
-  //     calc_t(k, j);
-  //   }
-  // }
-  // result = dp[N + M - 2][M - 1];
-#else
-  // if (T == 1) {
-  //   for (int k = 0; k < M + N - 1; k++) {
-  //     int L = MAX(0, k - N + 1), R = MIN(k + 1, M);
-  //     for (int j = L; j < R; j++) { 
-  //       calc_t(k, j);
-  //     }
-  //   }
-  // }
-  // else {
+}
+else {
   for (int k = 0; k < M + N - 1; k++) {
-    mutex_lock(&lock);
-    if (commit_cnt == T) {
-      commit_cnt = 0;
-      for (int i = 0; i < T; i++) {
-        flag[i] = 1;
-      }
-    }
-    mutex_unlock(&lock);
-    while (1) {
-      mutex_lock(&lock);
-      if (flag[id - 1]) {
-        flag[id - 1] = 0;
-        break;
-      }
-      mutex_unlock(&lock);
-    }
-    mutex_unlock(&lock);
     int L = MAX(0, k - N + 1), R = MIN(k + 1, M);
     int l = L + (R - L) / T * (id - 1), r = (id != T) ? (L + (R - L) / T * id) : R;
-    // printf("%d %d %d %d\n", L, R, l, r);
     for (int j = l; j < r; j++) { 
       calc_t(k, j);
     }
+    // for (int j = L + id - 1; j < R; j += T) {
+    //   calc_t(k, j);
+    // }
     mutex_lock(&lock);
-    commit_cnt++;
+    ++commit_cnt;
+    if (commit_cnt == T) {
+      commit_cnt = 0;
+      cond_broadcast(&cv);
+    }
+    else {
+      if (commit_cnt > 0)
+        cond_wait(&cv, &lock);
+    }
     mutex_unlock(&lock);
   }
   // }
   result = dp[N + M - 2][M - 1];
-#endif
-
+}
 }
 
 int main(int argc, char *argv[]) {

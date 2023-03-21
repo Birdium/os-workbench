02ea591e0d7d4184a6e1fea5d249acb2b25bb5d7
tracer-nju <tracer@nju.edu.cn>
2023-03-21 13:26:18 +0800

    Linux birdium-GL65-Leopard-10SFSK 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:26:18 up 6 min,  1 user,  load average: 0.92, 0.74, 0.36

--- .shadow/plcs/plcs.c
+++ .shadow/plcs/plcs.c
@@ -5,16 +5,16 @@
 #include "thread.h"
 #include "thread-sync.h"
 
-#define MAXN 15000
+#define MAXN 10000
 int T, N, M;
 char A[MAXN + 1], B[MAXN + 1];
-int dp[MAXN * 2][MAXN];
+int dp[MAXN][MAXN];
 int result;
 
 mutex_t lock = MUTEX_INIT();
+cond_t cv = COND_INIT();
 
 int commit_cnt = 0;
-int flag[4] = {1, 1, 1, 1};
 
 #define DP(x, y) (((x) >= 0 && (y) >= 0) ? dp[x][y] : 0)
 #define MAX(x, y) (((x) > (y)) ? (x) : (y))
@@ -29,12 +29,12 @@ inline void calc(int i, int j) {
   dp[i][j] = MAX3(skip_a, skip_b, take_both);
 }
 
-inline void calc_t(int i, int j) {
-  int skip_a = DP(i - 1, j);
-  int skip_b = DP(i - 1, j - 1);
-  int take_both = DP(i - 2, j - 1) + (A[i - j] == B[j]);
-  dp[i][j] = MAX3(skip_a, skip_b, take_both);
-}
+// inline void calc_t(int i, int j) {
+//   int skip_a = DP(i - 1, j);
+//   int skip_b = DP(i - 1, j - 1);
+//   int take_both = DP(i - 2, j - 1) + (A[i - j] == B[j]);
+//   dp[i][j] = MAX3(skip_a, skip_b, take_both);
+// }
 
 void Tworker(int id) {
   if (id > T) {
@@ -50,53 +50,30 @@ void Tworker(int id) {
     }
   }
   result = dp[N - 1][M - 1];
-  // for (int k = 0; k < M + N - 1; k++) {
-  //   int L = MAX(0, k - N + 1), R = MIN(k + 1, M);
-  //   for (int j = L; j < R; j++) { 
-  //     calc_t(k, j);
-  //   }
-  // }
-  // result = dp[N + M - 2][M - 1];
 #else
-  // if (T == 1) {
-  //   for (int k = 0; k < M + N - 1; k++) {
-  //     int L = MAX(0, k - N + 1), R = MIN(k + 1, M);
-  //     for (int j = L; j < R; j++) { 
-  //       calc_t(k, j);
-  //     }
-  //   }
-  // }
-  // else {
   for (int k = 0; k < M + N - 1; k++) {
-    mutex_lock(&lock);
-    if (commit_cnt == T) {
-      commit_cnt = 0;
-      for (int i = 0; i < T; i++) {
-        flag[i] = 1;
-      }
-    }
-    mutex_unlock(&lock);
-    while (1) {
-      mutex_lock(&lock);
-      if (flag[id - 1]) {
-        flag[id - 1] = 0;
-        break;
-      }
-      mutex_unlock(&lock);
-    }
-    mutex_unlock(&lock);
     int L = MAX(0, k - N + 1), R = MIN(k + 1, M);
     int l = L + (R - L) / T * (id - 1), r = (id != T) ? (L + (R - L) / T * id) : R;
-    // printf("%d %d %d %d\n", L, R, l, r);
     for (int j = l; j < r; j++) { 
-      calc_t(k, j);
+      calc(k - j, j);
     }
+    // for (int j = L + id - 1; j < R; j += T) {
+    //   calc(k - j, j);
+    // }
     mutex_lock(&lock);
-    commit_cnt++;
+    ++commit_cnt;
+    if (commit_cnt == T) {
+      commit_cnt = 0;
+      cond_broadcast(&cv);
+    }
+    else {
+      if (commit_cnt > 0)
+        cond_wait(&cv, &lock);
+    }
     mutex_unlock(&lock);
   }
   // }
-  result = dp[N + M - 2][M - 1];
+  result = dp[N - 1][M - 1];
 #endif
 
 }

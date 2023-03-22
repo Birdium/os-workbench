6a43e6474efb822d087cd9836e7515102c837837
tracer-nju <tracer@nju.edu.cn>
2023-03-22 22:21:52 +0800

    Linux birdium-ms7c94 5.15.102-1-MANJARO #1 SMP PREEMPT Mon Mar 13 11:02:38 UTC 2023 x86_64 GNU/Linux  22:21:52 up 4 min,  3 users,  load average: 0.38, 0.47, 0.23

--- .shadow/abstract-machine/am/src/x86/qemu/mpe.c
+++ .shadow/abstract-machine/am/src/x86/qemu/mpe.c
@@ -13,8 +13,9 @@ static void call_user_entry() {
 bool mpe_init(void (*entry)()) {
   user_entry = entry;
   boot_record()->jmp_code = 0x000bfde9; // (16-bit) jmp (0x7c00)
+
+  printf("%d\n", __am_ncpu);
   for (int cpu = 1; cpu < __am_ncpu; cpu++) {
-    printf("%d\n", 114);
     boot_record()->is_ap = 1;
     __am_lapic_bootap(cpu, (void *)boot_record());
     while (xchg(&ap_ready, 0) != 1) {

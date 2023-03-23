92d06ee97facb08c26387cfc6d31ccee185f227c
tracer-nju <tracer@nju.edu.cn>
2023-03-23 16:08:50 +0800

    Linux birdium-GL65-Leopard-10SFSK 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:08:50 up 19:05,  1 user,  load average: 1.03, 0.75, 0.83

--- .shadow/plcs/plcs.c
+++ .shadow/plcs/plcs.c
@@ -4,14 +4,12 @@
 #include "thread.h"
 #include "thread-sync.h"
 
-#define MAXN 10000
-#define MINN 3000
+#define MAXN 15000
+#define MINN 1000
 int T, N, M;
 char A[MAXN + 1], B[MAXN + 1];
-int dp[MAXN][MAXN];
+int dp[MAXN * 2][MAXN];
 int result;
-
-sem_t sem[16];
 
 mutex_t lock = MUTEX_INIT();
 cond_t cv = COND_INIT();
@@ -43,18 +41,10 @@ void Tworker(int id) {
     int L = MAX(0, k - N + 1), R = MIN(k + 1, M);
     int l = L + (R - L) / T * (id - 1), r = (id != T) ? (L + (R - L) / T * id) : R;
     for (int j = l; j < r; j++) { 
-      calc(k - j, j);
+      calc_t(k, j);
     }
     // for (int j = L + id - 1; j < R; j += T) {
     //   calc_t(k, j);
-    // }
-    // for (int i = 1; i <= T; i++) {
-    //   if (i != id) {
-    //     V(&sem[i - 1]);
-    //   }
-    // }
-    // for (int i = 1; i < T; i++) {
-    //   P(&sem[id - 1]);
     // }
     mutex_lock(&lock);
     ++commit_cnt;
@@ -89,15 +79,10 @@ int main(int argc, char *argv[]) {
   // clock_t start, end;
   // start = clock();
 #endif
-
-  for (int i = 0; i < T; i++) {
-    SEM_INIT(&sem[i], 0);
-  }
-
   for (int k = 0; k < MIN(MINN, M+N-1); k++) {
     int L = MAX(0, k - N + 1), R = MIN(k + 1, M);
     for (int j = L; j < R; j++) { 
-      calc(k - j, j);
+      calc_t(k, j);
     }
   }
 
@@ -110,7 +95,7 @@ int main(int argc, char *argv[]) {
   for (int k = M + N - MINN - 1; k < M + N - 1; k++) {
     int L = MAX(0, k - N + 1), R = MIN(k + 1, M);
     for (int j = L; j < R; j++) { 
-      calc(k - j, j);
+      calc_t(k, j);
     }
   }
 
@@ -119,5 +104,5 @@ int main(int argc, char *argv[]) {
   // printf("time=%lf\n", (double)(end-start));
 #endif
 
-  printf("%d\n", dp[N - 1][M - 1]);
+  printf("%d\n", dp[N + M - 2][M - 1]);
 }

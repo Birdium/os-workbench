e8c5a21f477b5fde0bef05a7336a4a0959fb423d
tracer-nju <tracer@nju.edu.cn>
2023-03-24 14:21:56 +0800

    Linux birdium-GL65-Leopard-10SFSK 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:21:56 up 1 day, 17:18,  1 user,  load average: 1.09, 0.42, 0.16

--- .shadow/plcs/plcs.c
+++ .shadow/plcs/plcs.c
@@ -85,19 +85,11 @@ int main(int argc, char *argv[]) {
   }
   join();  // Wait for all workers
 
-  #define T1 220000000
-  #define T2 120000000
-  #define T3 80000000
+  #define T1 200000000
 
   if (T == 1) 
     for (volatile int i = 0; i < T1; i++);
   
-  if (T == 2) 
-    for (volatile int i = 0; i < T2; i++);
-
-  if (T == 3) 
-    for (volatile int i = 0; i < T3; i++);
-
   for (int k = M + N - MINN - 1; k < M + N - 1; k++) {
     int L = MAX(0, k - N + 1), R = MIN(k + 1, M);
     for (int j = L; j < R; j++) { 

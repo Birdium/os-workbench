2de73ff16e24263d72df3810a1544050ec351593
tracer-nju <tracer@nju.edu.cn>
2023-03-27 20:41:34 +0800

    Linux birdium-ms7c94 5.15.102-1-MANJARO #1 SMP PREEMPT Mon Mar 13 11:02:38 UTC 2023 x86_64 GNU/Linux  20:41:34 up 2 days,  5:05,  4 users,  load average: 0.81, 0.86, 0.78

---
+++ .shadow/kernel/test/test.c
@@ -0,0 +1,31 @@
+#include <common.h>
+#include <klib.h>
+
+enum ops { OP_ALLOC = 1, OP_FREE };
+
+struct malloc_op random_op() {
+    struct malloc_op result;
+    result.type = OP_ALLOC;
+    result.sz = 1024;
+    return result;
+}
+
+struct malloc_op {
+  enum ops type;
+  union { size_t sz; void *addr; };
+};
+
+void stress_test() {
+  while (1) {
+    struct malloc_op op = random_op();
+    switch (op.type) {
+      case OP_ALLOC: alloc_check(pmm->alloc(op.sz), op.sz); break;
+      case OP_FREE:  free(op.addr); break;
+    }
+  }
+}
+
+int main() {
+  os->init();
+  mpe_init(stress_test);
+}

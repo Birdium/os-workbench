3297e9f70c2984069c7b01fbb47de68c5c52727b
tracer-nju <tracer@nju.edu.cn>
2023-03-27 21:59:41 +0800

    Linux birdium-ms7c94 5.15.102-1-MANJARO #1 SMP PREEMPT Mon Mar 13 11:02:38 UTC 2023 x86_64 GNU/Linux  21:59:41 up 2 days,  6:23,  4 users,  load average: 1.00, 0.84, 0.74

--- .shadow/kernel/src/os.c
+++ .shadow/kernel/src/os.c
@@ -4,13 +4,23 @@ static void os_init() {
   pmm->init();
 }
 
+#ifndef TEST
 static void os_run() {
   // for (const char *s = "Hello World from CPU #*\n"; *s; s++) {
   //   putch(*s == '*' ? '0' + cpu_current() : *s);
   // }
-  // printf("Hello World from CPU #%d\n", cpu_current());
+  printf("Hello World from CPU #%d\n", cpu_current());
   while (1) ;
 }
+#else 
+static void os_run() {
+  // for (const char *s = "Hello World from CPU #*\n"; *s; s++) {
+  //   putch(*s == '*' ? '0' + cpu_current() : *s);
+  // }
+  printf("Testing\n");
+  while (1) ;
+}
+#endif
 
 MODULE_DEF(os) = {
   .init = os_init,

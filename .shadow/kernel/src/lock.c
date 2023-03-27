a5798d43f14de100866e61036e31b140859babce
tracer-nju <tracer@nju.edu.cn>
2023-03-27 22:13:19 +0800

    Linux birdium-ms7c94 5.15.102-1-MANJARO #1 SMP PREEMPT Mon Mar 13 11:02:38 UTC 2023 x86_64 GNU/Linux  22:13:19 up 2 days,  6:36,  4 users,  load average: 1.20, 1.10, 0.92

--- .shadow/kernel/src/lock.c
+++ .shadow/kernel/src/lock.c
@@ -1,10 +1,10 @@
 #include <common.h>
 #include <lock.h>
 
-void spin_lock(lock_t *lk) {
+void spin_lock(spinlock_t *lk) {
   while (atomic_xchg(&lk->status, 1));
 }
 
-void spin_unlock(lock_t *lk) {
+void spin_unlock(spinlock_t *lk) {
   atomic_xchg(&lk->status, 0);
 }

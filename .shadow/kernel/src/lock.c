433ab5bc37d758d0224006a185184b7628b69e56
tracer-nju <tracer@nju.edu.cn>
2023-04-06 16:27:44 +0800

    Linux birdium-GL65-Leopard-10SFSK 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:27:44 up  2:19,  1 user,  load average: 1.30, 0.77, 0.82

--- .shadow/kernel/src/lock.c
+++ .shadow/kernel/src/lock.c
@@ -1,13 +1,11 @@
 #ifndef TEST
 #include <lock.h>
-#include <stdatomic.h>
 #include <stdint.h>
-static int atomic_xchg(int *addr, int newval) {
-  return atomic_exchange((int *)addr, newval);
-}
+#include <common.h>
+
 void spin_lock(spinlock_t *lk) {
   while (1) {
-    intptr_t value = atomic_xchg(lk, 1);
+    int value = atomic_xchg(lk, 1);
     if (value == 0) {
       break;
     }

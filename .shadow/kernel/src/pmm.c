e2f4655f83012d1f03cd7b85a3a873dde9d2dd13
tracer-nju <tracer@nju.edu.cn>
2023-04-08 23:12:49 +0800

    Linux birdium-ms7c94 5.15.105-1-MANJARO #1 SMP PREEMPT Fri Mar 31 05:22:54 UTC 2023 x86_64 GNU/Linux  23:12:49 up 17 min,  3 users,  load average: 0.31, 0.26, 0.19

--- .shadow/kernel/src/pmm.c
+++ .shadow/kernel/src/pmm.c
@@ -5,13 +5,7 @@
 #endif
 
 static inline size_t align(size_t size) {
-  size--;
-  size |= size >> 1;
-  size |= size >> 2;
-  size |= size >> 4;
-  size |= size >> 8;
-  size |= size >> 16;
-  return size + 1;
+  return (-(size & (-size))) & size;
 }
 
 spinlock_t lk;

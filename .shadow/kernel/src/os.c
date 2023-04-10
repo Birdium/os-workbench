89eb7bb478686b21ee3d90b8a0ed8a7670a2197c
tracer-nju <tracer@nju.edu.cn>
2023-04-10 15:11:54 +0800

    Linux birdium-ms7c94 5.15.105-1-MANJARO #1 SMP PREEMPT Fri Mar 31 05:22:54 UTC 2023 x86_64 GNU/Linux  15:11:54 up 1 day, 16:16,  3 users,  load average: 1.39, 0.75, 0.62

--- .shadow/kernel/src/os.c
+++ .shadow/kernel/src/os.c
@@ -36,12 +36,12 @@ static void os_run() {
   test_alloc(2);
   test_alloc(4);
   test_alloc(8);
-  buddy_debug_print();
   // void *p1 = test_alloc(1024);
   void *p2 = test_alloc(1024 * 1024);
   void *p3 = test_alloc(1024 * 1024);
   void *p4 = test_alloc(1024 * 1024);
   void *p5 = test_alloc(1024 * 1024 + 1);
+  buddy_debug_print();
   printf("--------free-------\n");
   test_free(p2);
   test_free(p3); test_free(p4);

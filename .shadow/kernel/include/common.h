8f6af83a21a1d20f39e1e7bf99cd3da265c95759
tracer-nju <tracer@nju.edu.cn>
2023-04-11 00:24:11 +0800

    Linux birdium-ms7c94 5.15.105-1-MANJARO #1 SMP PREEMPT Fri Mar 31 05:22:54 UTC 2023 x86_64 GNU/Linux  00:24:11 up 2 days,  1:29,  3 users,  load average: 3.61, 3.05, 2.91

--- .shadow/kernel/include/common.h
+++ .shadow/kernel/include/common.h
@@ -31,8 +31,8 @@
 
 #ifdef DEBUG 
 #ifndef TEST
-#define LOG_INFO(fmt, ...)  //printf("LOG_INFO CPU #%d @ [%s][%d]: "fmt"\n", cpu_current(), __FUNCTION__, __LINE__, ##__VA_ARGS__)
-#define LOG_LOCK(fmt, ...)  //printf("\033[1;31mLOG_LOCK CPU #%d @ [%s][%d]: "fmt"\n\33[0m", cpu_current(), __FUNCTION__, __LINE__, ##__VA_ARGS__)
+#define LOG_INFO(fmt, ...)  printf("LOG_INFO CPU #%d @ [%s][%d]: "fmt"\n", cpu_current(), __FUNCTION__, __LINE__, ##__VA_ARGS__)
+#define LOG_LOCK(fmt, ...)  printf("\033[1;31mLOG_LOCK CPU #%d @ [%s][%d]: "fmt"\n\33[0m", cpu_current(), __FUNCTION__, __LINE__, ##__VA_ARGS__)
 #else
 #define LOG_INFO(fmt, ...)  printf("LOG_INFO @ [%s][%d]: "fmt"\n", __FUNCTION__, __LINE__, ##__VA_ARGS__)
 #define LOG_LOCK(fmt, ...)  printf("\033[1;31mLOG_LOCK @ [%s][%d]: "fmt"\n\33[0m\n", __FUNCTION__, __LINE__, ##__VA_ARGS__)

cd65e4e78a212abdc988c701d1b47e5700cf8484
tracer-nju <tracer@nju.edu.cn>
2023-04-10 22:32:18 +0800

    Linux birdium-ms7c94 5.15.105-1-MANJARO #1 SMP PREEMPT Fri Mar 31 05:22:54 UTC 2023 x86_64 GNU/Linux  22:32:18 up 1 day, 23:37,  3 users,  load average: 0.45, 0.54, 0.60

--- .shadow/kernel/src/buddy.c
+++ .shadow/kernel/src/buddy.c
@@ -129,7 +129,6 @@ void buddy_free(void *addr) {
     // can merge
     while (size_exp < MAX_ALLOC_SIZE_EXP) {
         TableEntry *sibling_tbe = SIBLING_TBE(tbe);
-        LOG_INFO("test1: %p, sib:%p", TBE_2_ADDR(tbe), SIBLING_ADDR(tbe));
         if (sibling_tbe->allocated || sibling_tbe->size != size_exp) 
             break;
         sibling_tbe->allocated = 1;

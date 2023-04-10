34d87f711334e47a926c7e9e478d87c2408c3b5f
tracer-nju <tracer@nju.edu.cn>
2023-04-10 23:11:51 +0800

    Linux birdium-ms7c94 5.15.105-1-MANJARO #1 SMP PREEMPT Fri Mar 31 05:22:54 UTC 2023 x86_64 GNU/Linux  23:11:51 up 2 days, 16 min,  3 users,  load average: 1.76, 2.17, 1.81

--- .shadow/kernel/src/buddy.c
+++ .shadow/kernel/src/buddy.c
@@ -96,7 +96,6 @@ void *buddy_fetch_best_chunk(int exp) {
 // just an allocator
 void *buddy_alloc(size_t size) {
     // assume alloc is aligned
-    LOG_INFO("allocating %d memory", size);
     int size_exp = PAGE_SIZE_EXP;
     while (size_exp < MAX_ALLOC_SIZE_EXP && (1 << size_exp) != size) 
         ++size_exp;

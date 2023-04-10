2bd88e4f9cd6cdd977cfeb8e82cd65e5da11b66a
tracer-nju <tracer@nju.edu.cn>
2023-04-10 22:32:45 +0800

    Linux birdium-ms7c94 5.15.105-1-MANJARO #1 SMP PREEMPT Fri Mar 31 05:22:54 UTC 2023 x86_64 GNU/Linux  22:32:45 up 1 day, 23:37,  3 users,  load average: 0.93, 0.66, 0.64

--- .shadow/kernel/src/os.c
+++ .shadow/kernel/src/os.c
@@ -55,12 +55,13 @@ static void os_run() {
     printf("CPU #%d Allocating in %x, %d byte(s) %x\n", cpu_current(), (uintptr_t)p, size, size);
     test_free(p);
     printf("%d\n", i);
+      buddy_debug_print();
     assert((size | ((uintptr_t)p == size + (uintptr_t)p)) || ((size-1) | (uintptr_t)p) == (size-1) + (uintptr_t)p);
   }
   // size_t size = 16 * 1024 * 1024;
   // void *p = pmm->alloc(size);
   // printf("CPU #%d Allocating in %x, %d byte(s) %x\n", cpu_current(), (uintptr_t)p, size, size);
-  buddy_debug_print();
+
   printf("Success!\n");
   while (1);
 }

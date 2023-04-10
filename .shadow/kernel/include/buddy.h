f47a9fb979d127cf865ff6effefb955545821ac4
tracer-nju <tracer@nju.edu.cn>
2023-04-10 15:09:28 +0800

    Linux birdium-ms7c94 5.15.105-1-MANJARO #1 SMP PREEMPT Fri Mar 31 05:22:54 UTC 2023 x86_64 GNU/Linux  15:09:28 up 1 day, 16:14,  3 users,  load average: 0.62, 0.58, 0.56

--- .shadow/kernel/include/buddy.h
+++ .shadow/kernel/include/buddy.h
@@ -43,6 +43,7 @@ void init_buddy();
 void buddy_insert(TableEntry *tbe);
 void buddy_delete(TableEntry *tbe);
 void *buddy_alloc(size_t size);
+void buddy_free(void *addr);
 void *buddy_fetch_best_chunk(int exp);
 void buddy_debug_print();
 

81ff10d27761d2b613e827804b3e44cf7061debc
tracer-nju <tracer@nju.edu.cn>
2023-04-11 21:23:01 +0800

    Linux birdium-ms7c94 5.15.105-1-MANJARO #1 SMP PREEMPT Fri Mar 31 05:22:54 UTC 2023 x86_64 GNU/Linux  21:23:01 up 2 days, 22:28,  3 users,  load average: 0.79, 0.69, 0.57

--- .shadow/kernel/test/common.h
+++ .shadow/kernel/test/common.h
@@ -34,7 +34,6 @@
 #ifdef DEBUG 
 #define LOG_INFO(fmt, ...)  printf("LOG_INFO CPU #%d @ [%s][%d]: "fmt"\n", cpu_current(), __FUNCTION__, __LINE__, ##__VA_ARGS__)
 #define LOG_LOCK(fmt, ...)  printf("\033[1;31mLOG_LOCK CPU #%d @ [%s][%d]: "fmt"\n\33[0m", cpu_current(), __FUNCTION__, __LINE__, ##__VA_ARGS__)
-#endif
 #else
 #define LOG_INFO(fmt, ...)  
 #define LOG_LOCK(fmt, ...) 

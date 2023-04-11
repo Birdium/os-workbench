9d301f3ac33e56102002ba65259fbeb951121394
tracer-nju <tracer@nju.edu.cn>
2023-04-11 20:56:14 +0800

    Linux birdium-ms7c94 5.15.105-1-MANJARO #1 SMP PREEMPT Fri Mar 31 05:22:54 UTC 2023 x86_64 GNU/Linux  20:56:14 up 2 days, 22:01,  3 users,  load average: 1.14, 0.38, 0.13

--- .shadow/kernel/test/common.h
+++ .shadow/kernel/test/common.h
@@ -4,7 +4,41 @@
 #include <thread.h>
 #include <thread-sync.h>
 
-#define PAGE_SIZE 4096
+#define ROUNDUP(a, sz)      ((((uintptr_t)a) + (sz) - 1) & ~((sz) - 1))
+#define ROUNDDOWN(a, sz)    ((((uintptr_t)a)) & ~((sz) - 1))
+
+#define MAX_ALLOC_SIZE_EXP 24
+#define MAX_ALLOC_SIZE (1 << MAX_ALLOC_SIZE_EXP)
+#define PAGE_SIZE_EXP 12
+#define PAGE_SIZE (1 << PAGE_SIZE_EXP)
+#define MAX_ALLOC_PAGE_NUM MAX_ALLOC_SIZE / PAGE_SIZE
+#define HEAP_SIZE (heap.end - heap.start)
+#define PAGE_NUM (HEAP_SIZE / PAGE_SIZE)
+
+// convert table entry's addr from the chunk's physical addr
+#define ADDR_2_TBN(addr) (((void *)addr - heap.start) / PAGE_SIZE)
+#define ADDR_2_TBE(addr) (table + ((void *)addr - heap.start) / PAGE_SIZE)
+#define TBN_2_ADDR(tbn) (heap.start + PAGE_SIZE * tbn) 
+#define TBE_2_ADDR(tbe) (heap.start + PAGE_SIZE * (tbe - table))
+
+#define SIBLING_ADDR(tbe) ((void*)(((uintptr_t) TBE_2_ADDR(tbe)) ^ (1 << tbe->size)))
+#define SIBLING_TBE(tbe) (ADDR_2_TBE(SIBLING_ADDR(tbe)))
+
+#define PARENT_ADDR(tbe) ((void*)(((uintptr_t) TBE_2_ADDR(tbe)) & (~(1 << tbe->size))))
+#define PARENT_TBE(tbe) (ADDR_2_TBE(PARENT_ADDR(tbe)))
+
+
+#ifdef DEBUG 
+#ifndef TEST
+#define LOG_INFO(fmt, ...)  printf("LOG_INFO CPU #%d @ [%s][%d]: "fmt"\n", cpu_current(), __FUNCTION__, __LINE__, ##__VA_ARGS__)
+#define LOG_LOCK(fmt, ...)  printf("\033[1;31mLOG_LOCK CPU #%d @ [%s][%d]: "fmt"\n\33[0m", cpu_current(), __FUNCTION__, __LINE__, ##__VA_ARGS__)
+#else
+#define LOG_INFO(fmt, ...)  printf("LOG_INFO @ [%s][%d]: "fmt"\n", __FUNCTION__, __LINE__, ##__VA_ARGS__)
+#define LOG_LOCK(fmt, ...)  printf("\033[1;31mLOG_LOCK @ [%s][%d]: "fmt"\n\33[0m\n", __FUNCTION__, __LINE__, ##__VA_ARGS__)
+#endif
+#else
+#define LOG_INFO(fmt, ...)  
+#define LOG_LOCK(fmt, ...)  
 
 typedef struct {
     void *start, *end;

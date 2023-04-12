0fb63b41f364d362c6b574643eb442d7fd788b54
tracer-nju <tracer@nju.edu.cn>
2023-04-12 15:25:54 +0800

    Linux birdium-ms7c94 5.15.105-1-MANJARO #1 SMP PREEMPT Fri Mar 31 05:22:54 UTC 2023 x86_64 GNU/Linux  15:25:54 up 3 days, 16:30,  3 users,  load average: 0.21, 0.24, 0.29

--- .shadow/plcs/thread-sync.h
+++ .shadow/plcs/thread-sync.h
@@ -1,33 +1,39 @@
 #include <semaphore.h>
 
-// Spinlock
-typedef int spinlock_t;
-#define SPIN_INIT() 0
+// // Spinlock
+// typedef int spinlock_t;
+// #define SPIN_INIT() 0
 
-static inline int atomic_xchg(volatile int *addr, int newval) {
-  int result;
-  asm volatile ("lock xchg %0, %1":
-    "+m"(*addr), "=a"(result) : "1"(newval) : "memory");
-  return result;
-}
+// static inline int atomic_xchg(volatile int *addr, int newval) {
+//   int result;
+//   asm volatile ("lock xchg %0, %1":
+//     "+m"(*addr), "=a"(result) : "1"(newval) : "memory");
+//   return result;
+// }
 
-void spin_lock(spinlock_t *lk) {
-  while (1) {
-    intptr_t value = atomic_xchg(lk, 1);
-    if (value == 0) {
-      break;
-    }
-  }
-}
-void spin_unlock(spinlock_t *lk) {
-  atomic_xchg(lk, 0);
-}
+// void spin_lock(spinlock_t *lk) {
+//   while (1) {
+//     intptr_t value = atomic_xchg(lk, 1);
+//     if (value == 0) {
+//       break;
+//     }
+//   }
+// }
+// void spin_unlock(spinlock_t *lk) {
+//   atomic_xchg(lk, 0);
+// }
+
+// // Mutex
+// typedef pthread_mutex_t mutex_t;
+// #define MUTEX_INIT() PTHREAD_MUTEX_INITIALIZER
+// void mutex_lock(mutex_t *lk)   { pthread_mutex_lock(lk); }
+// void mutex_unlock(mutex_t *lk) { pthread_mutex_unlock(lk); }
 
 // Mutex
-typedef pthread_mutex_t mutex_t;
-#define MUTEX_INIT() PTHREAD_MUTEX_INITIALIZER
-void mutex_lock(mutex_t *lk)   { pthread_mutex_lock(lk); }
-void mutex_unlock(mutex_t *lk) { pthread_mutex_unlock(lk); }
+typedef spinlock_t mutex_t;
+#define SPIN_INIT() PTHREAD_MUTEX_INITIALIZER
+void spin_lock(mutex_t *lk)   { pthread_mutex_lock(lk); }
+void spin_unlock(mutex_t *lk) { pthread_mutex_unlock(lk); }
 
 // Conditional Variable
 typedef pthread_cond_t cond_t;

ef3a1db7140ef82b255c4f5e1bb572bed40119c2
tracer-nju <tracer@nju.edu.cn>
2023-04-12 16:22:46 +0800

    Linux birdium-ms7c94 5.15.105-1-MANJARO #1 SMP PREEMPT Fri Mar 31 05:22:54 UTC 2023 x86_64 GNU/Linux  16:22:46 up 3 days, 17:27,  3 users,  load average: 0.76, 0.82, 0.97

--- .shadow/kernel/src/buddy.c
+++ .shadow/kernel/src/buddy.c
@@ -181,6 +181,8 @@ void *buddy_alloc(size_t size) {
 
 void buddy_free(void *addr) {
     TableEntry *tbe = ADDR_2_TBE(addr);
+    TableEntry *parent_tbe = PARENT_TBE(tbe);
+    spin_lock(&(parent_tbe->lock));
     assert(tbe->allocated == 1);
     int size_exp = tbe->size;
     LOG_INFO("freeing 2^(%d) memory from %p", size_exp, addr);
@@ -192,14 +194,10 @@ void buddy_free(void *addr) {
     // can merge
     while (size_exp < MAX_ALLOC_SIZE_EXP) {
         TableEntry *sibling_tbe = SIBLING_TBE(tbe);
-
-        spin_lock(&(sibling_tbe->lock));
         if (sibling_tbe->allocated || sibling_tbe->size != size_exp || sibling_tbe->is_slab) {
-            spin_unlock(&(sibling_tbe->lock));
             break;
         }
         sibling_tbe->allocated = 1;
-        spin_unlock(&(sibling_tbe->lock));
 
         LOG_INFO("sibling info: size: %d allocated: %d is_slab: %d", sibling_tbe->size, sibling_tbe->allocated, sibling_tbe->is_slab);
         buddy_delete(sibling_tbe);
@@ -212,15 +210,17 @@ void buddy_free(void *addr) {
         spin_lock(&(list->lock));
         LOG_LOCK("fetched %d", list - buddy);
 
+        spin_unlock(&(parent_tbe->lock));
         tbe = PARENT_TBE(tbe);
+        parent_tbe = PARENT_TBE(tbe);
+        spin_lock(&(parent_tbe->lock));
 
-        spin_lock(&(tbe->lock));
         tbe->size = ++size_exp;
-        spin_unlock(&(tbe->lock));
 
         assert(tbe->size >= PAGE_SIZE_EXP);
     }
     tbe->allocated = 0;
+    spin_unlock(&(parent_tbe->lock));
     buddy_insert(tbe);
     spin_unlock(&(list->lock));
     LOG_LOCK("released %d", list - buddy);

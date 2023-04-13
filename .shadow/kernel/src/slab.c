079e557c480dbdba8bd0431ace45c62dcbf47c06
tracer-nju <tracer@nju.edu.cn>
2023-04-13 15:34:19 +0800

    Linux birdium-ms7c94 5.15.105-1-MANJARO #1 SMP PREEMPT Fri Mar 31 05:22:54 UTC 2023 x86_64 GNU/Linux  15:34:19 up 55 min,  3 users,  load average: 1.17, 0.93, 0.79

--- .shadow/kernel/src/slab.c
+++ .shadow/kernel/src/slab.c
@@ -108,7 +108,7 @@ void *slab_alloc(size_t size) {
 	// finally requires buddy sys
 	slab_fetch_buddy(slab_idx, cpu);
 	result = slab_list_poll(&(list->local));
-	printf("%d\n", result);
+	printf("%d\n", slab[cpu][slab_idx].local.cnt);
 	return result;
 }
 

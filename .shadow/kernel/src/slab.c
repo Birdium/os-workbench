4aad28016838b37fa15e0d34e9d9775ce7552fcd
tracer-nju <tracer@nju.edu.cn>
2023-04-13 15:52:46 +0800

    Linux birdium-ms7c94 5.15.105-1-MANJARO #1 SMP PREEMPT Fri Mar 31 05:22:54 UTC 2023 x86_64 GNU/Linux  15:52:46 up  1:13,  3 users,  load average: 0.83, 0.57, 0.62

--- .shadow/kernel/src/slab.c
+++ .shadow/kernel/src/slab.c
@@ -15,7 +15,7 @@ void slab_fetch_buddy(int slab_idx, int cpu) {
 		tbe_iter->cpu_cnt = cpu;
 	}
 	void *addr_end = addr_start + PAGE_SIZE * BUDDY_FETCH_PAGE_NUM;
-	printf("[%p, %p)\n", addr_start, addr_end);
+	printf("[%p, %p), %d\n", addr_start, addr_end, slab_idx);
 	LOG_INFO("SLAB fetch buddy from %p to %p", addr_start, addr_end);
 	
 	SlabList *list = &slab[cpu][slab_idx];

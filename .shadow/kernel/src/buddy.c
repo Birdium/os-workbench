ffede0b4f1367b9fb754cd60ef02607e31fbaf35
tracer-nju <tracer@nju.edu.cn>
2023-04-13 14:55:41 +0800

    Linux birdium-ms7c94 5.15.105-1-MANJARO #1 SMP PREEMPT Fri Mar 31 05:22:54 UTC 2023 x86_64 GNU/Linux  14:55:41 up 16 min,  3 users,  load average: 0.88, 1.96, 1.34

--- .shadow/kernel/src/buddy.c
+++ .shadow/kernel/src/buddy.c
@@ -141,6 +141,9 @@ void *buddy_alloc(size_t size) {
         ++size_exp;
     LOG_INFO("allocating 2^(%d) memory", size_exp);
     void *result = buddy_fetch_best_chunk(size_exp);
+    if ((uintptr_t)result == 0x102800) {
+        printf("%d\n", 102800);
+    }
     if (result == NULL) return NULL;
     TableEntry *tbe = ADDR_2_TBE(result);
     LOG_INFO("fetched page start from %p with size %d", result, (1<<tbe->size));

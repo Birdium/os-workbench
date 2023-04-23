5c7136c9f4f263a25653c97a4728866ed242de1d
tracer-nju <tracer@nju.edu.cn>
2023-04-23 23:25:35 +0800

    Linux birdium-ms7c94 5.15.108-1-MANJARO #1 SMP PREEMPT Thu Apr 20 11:41:14 UTC 2023 x86_64 GNU/Linux  23:25:35 up 1 day,  7:49,  3 users,  load average: 1.58, 1.25, 1.05

--- .shadow/sperf/sperf.c
+++ .shadow/sperf/sperf.c
@@ -2,21 +2,26 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-int main(int argc, char *argv[]) {
-  char *exec_argv[] = { "strace", "ls", NULL, };
-  char *exec_envp[] = {  NULL, };
-  execve("strace",          exec_argv, exec_envp);
-  execve("/bin/strace",     exec_argv, exec_envp);
-  execve("/usr/bin/strace", exec_argv, exec_envp);
-  // char *command = argv[1];
+int main(int argc, char *argv[], char *envp[]) {
+  // char *exec_argv[] = { "strace", "ls", NULL, };
+  // char *exec_envp[] = { "PATH=/bin", NULL, };
+  // execve("strace",          exec_argv, exec_envp);
+  // execve("/bin/strace",     exec_argv, exec_envp);
+  // execve("/usr/bin/strace", exec_argv, exec_envp);
+  char *command = argv[1];
   // printf("%s\n", command);
-  // int pid = fork();
-  // if (pid == 0) { // subproc
-  //   execve(command, argv + 1, )
-  // } 
-  // else {
-
-  // }
+  int fildes[2];
+  if (pipe(fildes) != 0) {
+    exit(-1);
+  }
+  int pid = fork();
+  if (pid == 0) { // subproc
+    execve(command, argv + 1, envp);
+    assert(0);
+  } 
+  else {
+    
+  }
   perror(argv[0]);
   exit(EXIT_FAILURE);
 }

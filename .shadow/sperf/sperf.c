bffbff26d903de954d42e93add561684271500dd
tracer-nju <tracer@nju.edu.cn>
2023-04-23 23:13:56 +0800

    Linux birdium-ms7c94 5.15.108-1-MANJARO #1 SMP PREEMPT Thu Apr 20 11:41:14 UTC 2023 x86_64 GNU/Linux  23:13:56 up 1 day,  7:38,  2 users,  load average: 1.04, 0.85, 0.87

--- .shadow/sperf/sperf.c
+++ .shadow/sperf/sperf.c
@@ -3,11 +3,13 @@
 #include <unistd.h>
 
 int main(int argc, char *argv[]) {
-  char *exec_argv[] = { "strace", "ls", NULL, };
-  char *exec_envp[] = { "PATH=/bin", NULL, };
-  execve("strace",          exec_argv, exec_envp);
-  execve("/bin/strace",     exec_argv, exec_envp);
-  execve("/usr/bin/strace", exec_argv, exec_envp);
+  // char *exec_argv[] = { "strace", "ls", NULL, };
+  // char *exec_envp[] = { "PATH=/bin", NULL, };
+  // execve("strace",          exec_argv, exec_envp);
+  // execve("/bin/strace",     exec_argv, exec_envp);
+  // execve("/usr/bin/strace", exec_argv, exec_envp);
+  char *command = argv[1];
+  printf("%s\n", command);
   perror(argv[0]);
   exit(EXIT_FAILURE);
 }

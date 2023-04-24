99c0c5e345dc4b9eb9af1bdd1d8094c411ae2e1e
tracer-nju <tracer@nju.edu.cn>
2023-04-24 23:44:28 +0800

    Linux birdium-ms7c94 5.15.108-1-MANJARO #1 SMP PREEMPT Thu Apr 20 11:41:14 UTC 2023 x86_64 GNU/Linux  23:44:28 up 2 days,  8:08,  3 users,  load average: 6.00, 5.96, 5.82

--- .shadow/kernel/src/kmt.c
+++ .shadow/kernel/src/kmt.c
@@ -1,9 +1,48 @@
 #include <os.h>
 
 static void kmt_init() {
+    //TODO: kmt init
+}
 
+static int kmt_create(task_t *task, const char *name, void (*entry)(void *arg), void *arg) {
+    //TODO: kmt create
+    return 0;
+}
+
+static void kmt_teardown(task_t *task) {
+    //TODO: kmt teardown
+}
+
+static void kmt_sem_init(sem_t *sem, const char *name, int value) {
+    //TODO: sem init
+}
+
+static void kmt_sem_signal(sem_t *sem) {
+    //TODO: sem signal
+}
+
+static void kmt_sem_wait(sem_t *sem) {
+    //TODO: sem wait
+}
+
+static void kmt_spin_init(spinlock_t *lk, const char *name) {
+    //TODO: spin init
+}
+static void kmt_spin_lock(spinlock_t *lk) {
+    //TODO: spin lock
+}
+static void kmt_spin_unlock(spinlock_t *lk) {
+    //TODO: spin unlock
 }
 
 MODULE_DEF(kmt) = {
     .init = kmt_init,
+    .create = kmt_create,
+    .teardown = kmt_teardown,
+    .sem_init = kmt_sem_init,
+    .sem_signal = kmt_sem_signal,
+    .sem_wait = kmt_sem_wait,
+    .spin_init = kmt_spin_init,
+    .spin_lock = kmt_spin_lock,
+    .spin_unlock = kmt_spin_unlock,
 };

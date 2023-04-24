004f4f4b6e8e8d49f404c92b946879d76d4dbba9
tracer-nju <tracer@nju.edu.cn>
2023-04-24 20:19:18 +0800

    Linux birdium-ms7c94 5.15.108-1-MANJARO #1 SMP PREEMPT Thu Apr 20 11:41:14 UTC 2023 x86_64 GNU/Linux  20:19:18 up 2 days,  4:43,  6 users,  load average: 5.52, 5.81, 5.89

--- .shadow/sperf/sperf.c
+++ .shadow/sperf/sperf.c
@@ -50,7 +50,6 @@ void list_update(char *name, double time) {
   }
   while (p) {
     if (strcmp(p->name, name) == 0) {
-      printf("%s %s\n", p->name, name);
       p->time += time;
       list_bubble(p);
       return;

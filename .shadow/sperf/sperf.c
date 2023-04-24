5eb239e1d92b375ea525cd6c09c0a38206f9bc60
tracer-nju <tracer@nju.edu.cn>
2023-04-24 19:39:23 +0800

    Linux birdium-ms7c94 5.15.108-1-MANJARO #1 SMP PREEMPT Thu Apr 20 11:41:14 UTC 2023 x86_64 GNU/Linux  19:39:23 up 2 days,  4:03,  5 users,  load average: 5.64, 5.75, 5.81

--- .shadow/sperf/sperf.c
+++ .shadow/sperf/sperf.c
@@ -37,6 +37,7 @@ Node *newNode(char *name, double time) {
 
 void list_update(char *name, double time) {
   tot_time += time;
+  printf("%s %lf\n", name, time);
   Node *p = head;
   if (p == NULL) {
     head = tail = newNode(name, time);

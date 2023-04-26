fbfccb77b90efef9c3bc9c56570b2762a4912549
Birdium <bibishu2001@163.com>
2023-04-26 16:46:57 +0800

    list update

--- kernel/include/list.h
+++ kernel/include/list.h
@@ -2,8 +2,6 @@
 #define LIST_H
 
 #include <stddef.h>
-#include <lock.h>
-
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -16,13 +14,15 @@
     typedef struct type##_list type##_list; \
     struct type##_list { \
         struct type##_list_node *head, *tail; \
-        void (*insert_prev) (type##_list *l, type##_list_node *p, type elem); \
-        void (*insert_next) (type##_list *l, type##_list_node *p, type elem); \
-        void (*push_front) (type##_list *l, type elem); \
-        void (*push_back) (type##_list *l, type elem); \
-        void (*remove) (type##_list *l, type##_list_node *p); \
-        void (*pop_front) (type##_list *l); \
-        void (*pop_back) (type##_list *l); \
+		type##_list *self; \
+        void (*insert_prev)	(type##_list *l, type##_list_node *p, type elem); \
+        void (*insert_next)	(type##_list *l, type##_list_node *p, type elem); \
+        void (*push_front)	(type##_list *l, type elem); \
+        void (*push_back)	(type##_list *l, type elem); \
+        void (*remove)		(type##_list *l, type##_list_node *p); \
+        void (*pop_front)	(type##_list *l); \
+        void (*pop_back)	(type##_list *l); \
+		void (*free)		(type##_list *l); \
     }; \
     static inline void type##_list_insert_prev(type##_list *l, type##_list_node *p, type elem) { \
         type##_list_node *q = malloc(sizeof(type##_list_node)); \
@@ -78,22 +78,33 @@
     static inline void type##_list_pop_back(type##_list *l) { \
         type##_list_remove(l, l->tail); \
     } \
+	static inline void type##_list_free(type##_list *l) { \
+		type##_list_node *p = l->head; \
+		while (p) { \
+			type##_list_node *q = p->next; \
+			free(p); \
+			p = q; \
+		} \
+		free(l); \
+	} \
     static inline type##_list *type##_list_init() { \
         type##_list *l = malloc(sizeof(type##_list)); \
+		l->self = l; \
         l->head = l->tail = NULL; \
-        l->insert_prev = type##_list_insert_prev; \
-        l->insert_next = type##_list_insert_next; \
-        l->push_front = type##_list_push_front; \
-        l->push_back = type##_list_push_back; \
-        l->pop_front = type##_list_pop_front; \
-        l->pop_back = type##_list_pop_back; \
+        l->insert_prev	= type##_list_insert_prev; \
+        l->insert_next	= type##_list_insert_next; \
+        l->push_front	= type##_list_push_front; \
+        l->push_back	= type##_list_push_back; \
+        l->pop_front	= type##_list_pop_front; \
+        l->pop_back 	= type##_list_pop_back; \
+		l->free 		= type##_list_free; \
         return l; \
     } \
 
 #define LIST_INIT(type, l) \
     type##_list *l = type##_list_init()
 
-#define foreach_list(type, it, list) \
+#define for_list(type, it, list) \
     for (type##_list_node *it = (list)->head; it != NULL; it = it->next)
-    
+
 #endif

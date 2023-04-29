6d7b9a77d645051bdacb86cbd54f2c081fd2c75d
tracer-nju <tracer@nju.edu.cn>
2023-04-29 19:05:46 +0800

    Linux birdium-GL65-Leopard-10SFSK 5.19.0-40-generic #41~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 31 16:00:14 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:05:46 up 2 days,  5:05,  1 user,  load average: 1.53, 0.81, 0.52

--- .shadow/kernel/src/kmt/sem.c
+++ .shadow/kernel/src/kmt/sem.c
@@ -1,10 +1,11 @@
+#include "list.h"
 #include <kmt.h>
 
 void kmt_sem_init(sem_t *sem, const char *name, int value) {
 	sem->name = name;
 	sem->cnt = value;
 	kmt->spin_init(&sem->lk, name);
-	
+	// LIST_INIT(task_t, sem->)
 }
 
 void kmt_sem_signal(sem_t *sem) {

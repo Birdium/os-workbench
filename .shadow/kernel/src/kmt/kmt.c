795adae28650915a9628e1f431cb81f7cdb53944
tracer-nju <tracer@nju.edu.cn>
2023-05-08 21:27:14 +0800

    Linux birdium-GL65-Leopard-10SFSK 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:27:14 up 5 days, 23:18,  1 user,  load average: 1.17, 1.02, 0.80

--- .shadow/kernel/src/kmt/kmt.c
+++ .shadow/kernel/src/kmt/kmt.c
@@ -36,7 +36,6 @@ static Context *kmt_schedule(Event ev, Context *context) {
     int cpu = cpu_current();
     panic_on(cur_task == NULL, "no available task");
     LOG_INFO("current task: %s, status %d", cur_task->name, cur_task->status);
-    panic_on(cur_task->status != RUNNING, "current task not running");
     switch (ev.event) {
         case EVENT_YIELD: case EVENT_IRQ_TIMER:
         // schedule to other tasks

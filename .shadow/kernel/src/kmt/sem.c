6ef4f749152e198032b3649b1c226e0b2bd6b6f6
tracer-nju <tracer@nju.edu.cn>
2023-05-08 21:38:19 +0800

    Linux birdium-GL65-Leopard-10SFSK 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:38:19 up 5 days, 23:29,  1 user,  load average: 0.96, 0.77, 0.73

--- .shadow/kernel/src/kmt/sem.c
+++ .shadow/kernel/src/kmt/sem.c
@@ -28,8 +28,8 @@ void kmt_sem_signal(sem_t *sem) {
 		}
 		task_t *ntask = p->elem;
 		kmt->spin_lock(task_list_lk);
-		sem->tasks.remove(&sem->tasks, p);
 		LOG_INFO("%s %d", ntask->name, ntask->status);
+		sem->tasks.remove(&sem->tasks, p);
 		panic_on(ntask->status != SLEEPING, "waiting task not sleeping");
 		ntask->status = RUNNABLE;
 		task_list->push_back(task_list, ntask);

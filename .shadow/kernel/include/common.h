d53c8513130166dae4b8d7432a04f96d33440538
tracer-nju <tracer@nju.edu.cn>
2023-05-08 20:38:43 +0800

    Linux birdium-GL65-Leopard-10SFSK 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:38:43 up 5 days, 22:29,  1 user,  load average: 0.51, 0.72, 0.69

--- .shadow/kernel/include/common.h
+++ .shadow/kernel/include/common.h
@@ -45,4 +45,12 @@
 
 #endif
 
+#ifdef TRACE_F
+  #define TRACE_ENTRY printf("[trace] %s:entry\n", __func__)
+  #define TRACE_EXIT printf("[trace] %s:exit\n", __func__)
+#else
+  #define TRACE_ENTRY ((void)0)
+  #define TRACE_EXIT ((void)0)
+#endif
+
 #endif
--- .shadow/kernel/src/kmt/kmt.c
+++ .shadow/kernel/src/kmt/kmt.c
@@ -26,6 +26,7 @@ static Context *kmt_context_save(Event ev, Context *context) {
 }
 
 static Context *kmt_schedule(Event ev, Context *context) {
+    TRACE_ENTRY;
     int cpu = cpu_current();
     panic_on(cur_task == NULL, "no available task");
     switch (ev.event) {
@@ -42,6 +43,7 @@ static Context *kmt_schedule(Event ev, Context *context) {
         default:
             break;
     }
+    TRACE_EXIT;
     return current[cpu]->context;
 }
 

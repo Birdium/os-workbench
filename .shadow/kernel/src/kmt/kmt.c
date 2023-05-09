5e9e74affff2191560e73240f01bd2470f34028d
tracer-nju <tracer@nju.edu.cn>
2023-05-09 20:38:28 +0800

    Linux birdium-ms7c94 5.15.108-1-MANJARO #1 SMP PREEMPT Thu Apr 20 11:41:14 UTC 2023 x86_64 GNU/Linux  20:38:28 up 17 days,  5:02,  4 users,  load average: 7.12, 6.74, 6.53

--- .shadow/kernel/src/kmt/kmt.c
+++ .shadow/kernel/src/kmt/kmt.c
@@ -104,7 +104,7 @@ static int kmt_create(task_t *task, const char *name, void (*entry)(void *arg), 
         }, entry, arg
     );
     task->next = NULL;
-    LOG_INFO("%d", task_list_lk->cpu);
+    LOG_INFO("%d", cpu_current());
     kmt->spin_lock(task_list_lk);
     task_list->push_back(task_list, task);
     LOG_INFO("%d", task_list_lk->cpu);

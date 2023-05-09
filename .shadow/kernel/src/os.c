c950bdf9880f3b1e3a602b1b517ae296a4bfd645
tracer-nju <tracer@nju.edu.cn>
2023-05-10 00:37:50 +0800

    Linux birdium-ms7c94 5.15.108-1-MANJARO #1 SMP PREEMPT Thu Apr 20 11:41:14 UTC 2023 x86_64 GNU/Linux  00:37:50 up 17 days,  9:01,  3 users,  load average: 6.88, 6.66, 6.57

--- .shadow/kernel/src/os.c
+++ .shadow/kernel/src/os.c
@@ -99,7 +99,7 @@ Context *os_trap(Event ev, Context *context) {
   }
   panic_on(!next, "returning NULL context");
   panic_on(!sane_context(next), "returning to invalid context");
-  LOG_INFO("trap returning %p with rip %d", next, next->rip);
+  LOG_INFO("trap returning %p with rip %x", next, next->rip);
   TRACE_EXIT;
   return next;
 }

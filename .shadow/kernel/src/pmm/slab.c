e1f6a47f2a6e728334a00e7918a3ac63221a26cf
tracer-nju <tracer@nju.edu.cn>
2023-05-09 21:23:07 +0800

    Linux birdium-ms7c94 5.15.108-1-MANJARO #1 SMP PREEMPT Thu Apr 20 11:41:14 UTC 2023 x86_64 GNU/Linux  21:23:07 up 17 days,  5:47,  4 users,  load average: 6.31, 6.35, 6.38

--- .shadow/kernel/src/pmm/slab.c
+++ .shadow/kernel/src/pmm/slab.c
@@ -105,7 +105,7 @@ void *slab_alloc(size_t size) {
     int size_exp = 0;
     while (size_exp < PAGE_SIZE_EXP && (1 << size_exp) != size) 
         ++size_exp;
-    LOG_INFO("allocating 2^(%d) memory", size_exp);
+    LOG_TRACE("allocating 2^(%d) memory", size_exp);
 
 	// first try local list
 	int cpu = cpu_current();

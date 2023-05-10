7e32215e2eb791f912dc8802d52cc089717b35af
tracer-nju <tracer@nju.edu.cn>
2023-05-10 18:54:35 +0800

    Linux birdium-ms7c94 5.15.108-1-MANJARO #1 SMP PREEMPT Thu Apr 20 11:41:14 UTC 2023 x86_64 GNU/Linux  18:54:35 up 18 days,  3:18,  4 users,  load average: 6.95, 6.81, 6.74

--- .shadow/kernel/src/os.c
+++ .shadow/kernel/src/os.c
@@ -13,9 +13,9 @@ task_t *task_alloc() {
 sem_t empty, fill;
 #define P kmt->sem_wait
 #define V kmt->sem_signal
-#define N 2
-#define NPROD 5
-#define NCONS 5
+#define N 5
+#define NPROD 10
+#define NCONS 10
 
 void Tproduce(void *arg) { while (1) { P(&empty); putch('('); V(&fill);  } }
 void Tconsume(void *arg) { while (1) { P(&fill);  putch(')'); V(&empty); } }

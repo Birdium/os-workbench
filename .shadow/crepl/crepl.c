6db1343d9d747c4bcc7541494c15363313904542
tracer-nju <tracer@nju.edu.cn>
2023-05-23 22:40:48 +0800

    Linux birdium-ms7c94 5.15.109-1-MANJARO #1 SMP PREEMPT Wed Apr 26 19:16:58 UTC 2023 x86_64 GNU/Linux  22:40:48 up 2 days,  1:01,  5 users,  load average: 1.09, 1.10, 1.04

--- .shadow/crepl/crepl.c
+++ .shadow/crepl/crepl.c
@@ -1,5 +1,92 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <unistd.h>
+#include <dlfcn.h>
+
+bool is_Func(char *str) {
+  if (strlen(str) <= 2) return false;
+  return str[0] == 'i' && str[1] == 'n' && str[2] == 't';
+}
+
+char src_filename[128], dst_filename[128], func_name[128];
+
+void extract_func_name(char *line) {
+  char *p = line + 3, *q = func_name;
+  while (*p && !is_space(*p)) {
+    p++;
+  }
+  while (*p && is_space(*p)) {
+    *q = *p; 
+    p++; q++;
+  }
+  *q = '\0';
+}
+
+static int expr_cnt = 0;
+
+int comp_func(char *line) {
+  bool is_func = is_Func(line);
+  if (is_func) {
+    extract_func_name(line);
+  }
+  else {
+    sprintf(func_name, "__expr_wrapper_%d", expr_cnt);
+  }
+  sprintf(src_filename, "/tmp/crepl_%s.c-XXXXXX", func_name);
+  sprintf(dst_filename, "/tmp/crepl_%s.so-XXXXXX", func_name);
+  if (mkstemp(src_filename) < 0 || mkstemp(dst_filename) < 0) {
+    perror("mkstemp failed!");
+    return -1;
+  }
+  FILE *src = fopen(src_filename, "w");
+  if (src == NULL) {
+    perror("failed to create src file");
+  }
+  if (is_func) {
+    fprintf(src, "%s\n", line);
+  }
+  else {
+    fprintf(src, "int %s() { return %s; } \n", func_name, line);
+  }
+  fclose(src);
+  char new_argv[] = {
+    "gcc", 
+    "-shared",
+    "-fPIC", 
+    "-o",
+    dst_filename,
+    src_filename,
+    "-ldl",
+    NULL
+  };
+  int pid = fork();
+  if (pid == 0) { // son
+    execvp("gcc", new_argv);
+    assert(0); // should not reach here
+  }
+  else {
+    wait(NULL);
+    void *dl = dlopen(dst_filename, RTLD_NOW);
+    if (dl == NULL) {
+      perror("dlopen failed");
+      return -1;
+    }
+    if (is_func) {
+      printf("OK.\n");
+    }
+    else {
+      int (*expr)() = dlsym(dl, func_name);
+      printf("= %d.\n", expr());
+    }
+  }
+}
+
+void comp_expr(char *line) {
+
+}
 
 int main(int argc, char *argv[]) {
   static char line[4096];
@@ -9,6 +96,7 @@ int main(int argc, char *argv[]) {
     if (!fgets(line, sizeof(line), stdin)) {
       break;
     }
-    printf("Got %zu chars.\n", strlen(line)); // ??
+    // printf("Got %zu chars.\n", strlen(line)); // ??
+    comp_func(line);
   }
 }

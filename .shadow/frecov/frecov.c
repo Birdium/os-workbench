06b5aab7ff3c27dc845d88b2d4103b40756a8438
tracer-nju <tracer@nju.edu.cn>
2023-06-27 17:56:19 +0800

    Linux Birdium-Laptop 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  17:56:19 up  1:58,  1 user,  load average: 0.24, 0.11, 0.02

--- .shadow/frecov/frecov.c
+++ .shadow/frecov/frecov.c
@@ -61,6 +61,8 @@ int main(int argc, char *argv[]) {
   struct fat32hdr *hdr = map_disk(argv[1]);
 
   // TODO: frecov
+  int RootDirSectors = ((hdr->BPB_RootEntCnt * 32) + (hdr->BPB_BytsPerSec – 1)) / hdr->BPB_BytsPerSec;
+  printf("%d\n", RootDirSectors);
 
   // file system traversal
   munmap(hdr, hdr->BPB_TotSec32 * hdr->BPB_BytsPerSec);

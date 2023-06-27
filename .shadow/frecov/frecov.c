109469e8365a6986b5be9c696bdb8bd33c4b2465
tracer-nju <tracer@nju.edu.cn>
2023-06-27 18:38:22 +0800

    Linux Birdium-Laptop 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  18:38:22 up  2:40,  1 user,  load average: 0.00, 0.02, 0.00

--- .shadow/frecov/frecov.c
+++ .shadow/frecov/frecov.c
@@ -69,7 +69,7 @@ int main(int argc, char *argv[]) {
   else TotSec = hdr->BPB_TotSec32;
   int DataSec = TotSec - (hdr->BPB_RsvdSecCnt + (hdr->BPB_NumFATs * FATSz) + RootDirSectors);
   int CountofClusters = DataSec / hdr->BPB_SecPerClus;
-  printf("%d %d\n", TotSec, DataSec);
+  printf("%d %d\n", TotSec, hdr->BPB_SecPerClus);
 
   // file system traversal
   munmap(hdr, hdr->BPB_TotSec32 * hdr->BPB_BytsPerSec);

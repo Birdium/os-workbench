4683a09d8a7b4096d595c400460cccb428a2a7de
tracer-nju <tracer@nju.edu.cn>
2023-06-27 18:00:18 +0800

    Linux Birdium-Laptop 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  18:00:18 up  2:02,  1 user,  load average: 0.00, 0.05, 0.00

--- .shadow/frecov/frecov.c
+++ .shadow/frecov/frecov.c
@@ -62,12 +62,12 @@ int main(int argc, char *argv[]) {
 
   // TODO: frecov
   int RootDirSectors = ((hdr->BPB_RootEntCnt * 32) + (hdr->BPB_BytsPerSec - 1)) / hdr->BPB_BytsPerSec;
-  int FATSz = 0;
+  int FATSz = 0, TotSec = 0;
   if (hdr->BPB_FATSz16 != 0) FATSz = hdr->BPB_FATSz16;
   else FATSz = hdr->BPB_FATSz32;
-  if (hdr->BPB_TotSec16 != 0) FATSz = hdr->BPB_TotSec16;
-  else FATSz = hdr->BPB_TotSec32;
-  int DataSec = hdr->TotSec - (hdr->BPB_RsvdSecCnt + (hdr->BPB_NumFATs * FATSz) + RootDirSectors);
+  if (hdr->BPB_TotSec16 != 0) TotSec = hdr->BPB_TotSec16;
+  else TotSec = hdr->BPB_TotSec32;
+  int DataSec = TotSec - (hdr->BPB_RsvdSecCnt + (hdr->BPB_NumFATs * FATSz) + RootDirSectors);
   int CountofClusters = DataSec / hdr->BPB_SecPerClus;
   printf("%d\n", CountofClusters);
 

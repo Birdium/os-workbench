4d0c01494ad60ec23b0f53c636367f3bc63a5702
tracer-nju <tracer@nju.edu.cn>
2023-08-02 15:58:02 +0800

    Linux Birdium-Laptop 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:58:02 up 13:11,  1 user,  load average: 0.08, 0.02, 0.01

--- .shadow/kernel/src/uproc.c
+++ .shadow/kernel/src/uproc.c
@@ -3,5 +3,16 @@
 
 #include "initcode.inc"
 
+void init() {
+	vme_init((void * (*)(int))pmm->alloc, pmm->free);
+}
+
+int kputc(task_t *task, char ch) {
+  putch(ch); // safe for qemu even if not lock-protected
+  return 0;
+}
+
 MODULE_DEF(uproc) = {
+	.init = init,
+	.kputc = kputc,
 };

a9e66d0d69d3abcc14452322d2a4495ed0362636
tracer-nju <tracer@nju.edu.cn>
2023-08-05 20:12:16 +0800

    Linux Birdium-Laptop 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  20:12:16 up  7:12,  1 user,  load average: 0.17, 0.10, 0.03

--- .shadow/user/init.c
+++ .shadow/user/init.c
@@ -1,11 +1,12 @@
 #include "ulib.h"
+#include <string.h>
 
 int main() {
   // Example:
   // printf("pid = %d\n", getpid());
   int p = getpid();
   while(1){
-    // uptime();
+    uptime();
   }
   // char buf[10];
   // memset(buf, 0, sizeof(buf));
@@ -16,5 +17,5 @@ int main() {
   // for (int n = m; n != 0; n /= 10, p /= 10) {
   //   kputc('0' + p / n);
   // }
-  return 0;
+  return p;
 }

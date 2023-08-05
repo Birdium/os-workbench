ec8ca3c403b2b69e505ea8deb7d172882f698257
tracer-nju <tracer@nju.edu.cn>
2023-08-05 13:31:03 +0800

    Linux Birdium-Laptop 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  13:31:03 up 31 min,  1 user,  load average: 0.04, 0.04, 0.00

--- .shadow/kernel/src/uproc.c
+++ .shadow/kernel/src/uproc.c
@@ -42,7 +42,7 @@ static Context *pagefault_handler(Event ev, Context *context) {
 //   AddrSpace *as = cur_task->as;
 //   void *pa = pmm->alloc(as->pgsize);
 //   void *va = ;
-//   printf("%s, %p, %p\n", ev.msg, ev.cause, ev.ref);
+  printf("task: %s, %s, %p, %p\n", cur_task->name, ev.msg, ev.cause, ev.ref);
   return NULL;
 }
 
@@ -61,7 +61,7 @@ void init_alloc(task_t *init_task) {
 	void *pa = pmm->alloc(_init_len);
 	void *va = as->area.start;
 	for (int offset = 0; offset < align(_init_len); offset += as->pgsize) {
-		printf("%p <- %p, PROT: %d\n", va + offset, pa + offset, MMAP_READ | MMAP_WRITE);
+		printf("%s: %p <- %p, PROT: %d\n", init_task->name, va + offset, pa + offset, MMAP_READ | MMAP_WRITE);
 		map(as, va + offset, pa + offset, MMAP_READ | MMAP_WRITE);
 	}
 	return;

c34612ab6f34bebbd682389c67a90f8dc4d8fb5d
tracer-nju <tracer@nju.edu.cn>
2023-08-06 01:04:44 +0800

    Linux Birdium-Laptop 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  01:04:44 up 12:04,  1 user,  load average: 0.09, 0.24, 0.19

--- .shadow/kernel/src/uproc.c
+++ .shadow/kernel/src/uproc.c
@@ -149,9 +149,9 @@ int uproc_fork(task_t *father) {
 	memcpy(son->stack, father->stack, KMT_STACK_SIZE);
 	uintptr_t rsp0 = son->context->rsp0;
 	void *cr3 = son->context->cr3;
+	LOG_USER("%p %p %p %p", son->stack, son->context->rsp, father->stack, father->context->rsp);
 	son->context = father->context;
 	son->context->rsp = father->context->rsp + son->stack - father->stack;
-	LOG_USER("%p %p %p %p", son->stack, son->context->rsp, father->stack, father->context->rsp);
 	son->context->rsp0 = rsp0;
 	son->context->cr3 = cr3;
 	son->context->GPRx = 0;

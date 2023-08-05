c359afb6f817fcc795b9e4f19c2a366d1098a251
tracer-nju <tracer@nju.edu.cn>
2023-08-05 14:16:21 +0800

    Linux Birdium-Laptop 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  14:16:21 up  1:16,  1 user,  load average: 0.02, 0.02, 0.00

--- .shadow/abstract-machine/am/src/x86/qemu/vme.c
+++ .shadow/abstract-machine/am/src/x86/qemu/vme.c
@@ -132,7 +132,7 @@ void protect(AddrSpace *as) {
   }
   as->pgsize = mmu.pgsize;
   as->area   = uvm_area;
-  printf("%p\n", vm_areas[0].area.end);
+  printf("area %p\n", vm_areas[0].area.end);
   as->ptr    = (void *)((uintptr_t)upt | PTE_P | PTE_U);
 }
 

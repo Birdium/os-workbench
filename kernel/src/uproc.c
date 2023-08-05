9fbf2a2061b1c81cce29b4dc6a1c7be9de1efa51
Birdium <bibishu2001@163.com>
2023-08-05 20:53:46 +0800

    refactor

--- kernel/src/uproc.c
+++ kernel/src/uproc.c
@@ -1,9 +1,9 @@
 #include "uproc.h"
 #include <assert.h>
+#include <kmt.h>
 #include <os.h>
 #include <stdio.h>
 #include <syscall.h>
-#include <kmt.h>
 
 #include "common.h"
 #include "initcode.inc"
@@ -13,92 +13,93 @@ pid_entry_t pinfo[UPROC_PID_NUM];
 
 spinlock_t pid_lock;
 
-static int next_pid = 1; 
+static int next_pid = 1;
 
 static int pid_alloc() {
-	kmt->spin_lock(&pid_lock);
-	int pid = next_pid;
-	panic_on(pinfo[pid].valid, "NEXT_PID INVALID");
-	pinfo[pid].valid = 0;
-	for (next_pid = pid + 1; next_pid != pid; next_pid++) {
-		if (pinfo[next_pid].valid) {
-			break;
-		}
-		if (next_pid == UPROC_PID_NUM - 1) next_pid = 0;
-	}
-	panic_on(pinfo[next_pid].valid, "UPROC PID NUM RUNNING OUT");
-	kmt->spin_unlock(&pid_lock);
-	return pid;
+  kmt->spin_lock(&pid_lock);
+  int pid = next_pid;
+  panic_on(pinfo[pid].valid, "NEXT_PID INVALID");
+  pinfo[pid].valid = 0;
+  for (next_pid = pid + 1; next_pid != pid; next_pid++) {
+    if (pinfo[next_pid].valid) {
+      break;
+    }
+    if (next_pid == UPROC_PID_NUM - 1)
+      next_pid = 0;
+  }
+  panic_on(pinfo[next_pid].valid, "UPROC PID NUM RUNNING OUT");
+  kmt->spin_unlock(&pid_lock);
+  return pid;
 }
 
 static Context *syscall_handler(Event ev, Context *context) {
   // TODO: deal with syscall
   LOG_INFO("%d", ienabled());
+  switch (context->GPRx) {}
   return NULL;
 }
 
 static Context *pagefault_handler(Event ev, Context *context) {
-	// TODO: deal with pgflt
-	AddrSpace *as = &(cur_task->as);
-	int pg_mask = ~(as->pgsize-1);
-	void *pa = pmm->alloc(as->pgsize);
-	void *va = (void*)(ev.ref & pg_mask);
-	printf("task: %s, %s, %d\n", cur_task->name, ev.msg, ev.cause);
-	printf("%p %p %p(%p)\n", as, pa, va, ev.ref);
-	map(as, va, pa, MMAP_READ | MMAP_WRITE);
-	return NULL;
+  // TODO: deal with COW
+  AddrSpace *as = &(cur_task->as);
+  int pg_mask = ~(as->pgsize - 1);
+  void *pa = pmm->alloc(as->pgsize);
+  void *va = (void *)(ev.ref & pg_mask);
+  printf("task: %s, %s, %d\n", cur_task->name, ev.msg, ev.cause);
+  printf("%p %p %p(%p)\n", as, pa, va, ev.ref);
+  map(as, va, pa, MMAP_READ | MMAP_WRITE);
+  return NULL;
 }
 
 static inline size_t align(size_t size) {
-	size--;
-	size |= size >> 1;
-	size |= size >> 2;
-	size |= size >> 4;
-	size |= size >> 8;
-	size |= size >> 16;
-	return size + 1;
+  size--;
+  size |= size >> 1;
+  size |= size >> 2;
+  size |= size >> 4;
+  size |= size >> 8;
+  size |= size >> 16;
+  return size + 1;
 }
 
 void init_alloc(task_t *init_task) {
-	AddrSpace *as = &(init_task->as);
-	int pa_size = _init_len > as->pgsize ? _init_len : as->pgsize;
-	void *pa = pmm->alloc(pa_size);
-	void *va = as->area.start;
-	for (int offset = 0; offset < align(_init_len); offset += as->pgsize) {
-		printf("%s: %p <- %p, PROT: %d\n", init_task->name, va + offset, pa + offset, MMAP_READ | MMAP_WRITE);
-		map(as, va + offset, pa + offset, MMAP_READ | MMAP_WRITE);
-	}
-	memcpy(pa, _init, _init_len);
-	return;
+  AddrSpace *as = &(init_task->as);
+  int pa_size = _init_len > as->pgsize ? _init_len : as->pgsize;
+  void *pa = pmm->alloc(pa_size);
+  void *va = as->area.start;
+  for (int offset = 0; offset < align(_init_len); offset += as->pgsize) {
+    printf("%s: %p <- %p, PROT: %d\n", init_task->name, va + offset,
+           pa + offset, MMAP_READ | MMAP_WRITE);
+    map(as, va + offset, pa + offset, MMAP_READ | MMAP_WRITE);
+  }
+  memcpy(pa, _init, _init_len);
+  return;
 }
 
 void uproc_init() {
-	vme_init((void * (*)(int))pmm->alloc, pmm->free);
-	kmt->spin_init(&pid_lock, "pid lock");
-  	os->on_irq(0, EVENT_SYSCALL, syscall_handler);
-	os->on_irq(0, EVENT_PAGEFAULT, pagefault_handler);
-	for (int i = 1; i < UPROC_PID_NUM; i++) {
-		pinfo[i].valid = 0;
-	}
-	task_t *task = pmm->alloc(sizeof(task_t));
-	int pid = pid_alloc(); 
-	kmt_ucreate(task, "init", pid, 0);
-	init_alloc(task);
-	panic_on(pid != 1, "first uproc id not 1");
-	LOG_INFO("%p", task->context->rsp);
-	// TODO: finish init
+  vme_init((void *(*)(int))pmm->alloc, pmm->free);
+  kmt->spin_init(&pid_lock, "pid lock");
+  os->on_irq(0, EVENT_SYSCALL, syscall_handler);
+  os->on_irq(0, EVENT_PAGEFAULT, pagefault_handler);
+  for (int i = 1; i < UPROC_PID_NUM; i++) {
+    pinfo[i].valid = 0;
+  }
+  task_t *task = pmm->alloc(sizeof(task_t));
+  int pid = pid_alloc();
+  kmt_ucreate(task, "init", pid, 0);
+  init_alloc(task);
+  panic_on(pid != 1, "first uproc id not 1");
+  LOG_INFO("%p", task->context->rsp);
+  // TODO: finish init
 }
 
 int uproc_kputc(task_t *task, char ch) {
-  	putch(ch); // safe for qemu even if not lock-protected
-  	return 0;
+  putch(ch); // safe for qemu even if not lock-protected
+  return 0;
 }
 
-int uproc_getpid(task_t *task) {
-	return task->pid;
-}
+int uproc_getpid(task_t *task) { return task->pid; }
 
 MODULE_DEF(uproc) = {
-	.init = uproc_init,
-	.kputc = uproc_kputc,
+    .init = uproc_init,
+    .kputc = uproc_kputc,
 };

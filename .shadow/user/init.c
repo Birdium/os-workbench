36dbd7a71be4c57daceffa6b6e47bea6a9a07f4e
tracer-nju <tracer@nju.edu.cn>
2023-08-06 01:12:38 +0800

    Linux Birdium-Laptop 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  01:12:38 up 12:12,  1 user,  load average: 0.63, 0.34, 0.23

--- .shadow/user/init.c
+++ .shadow/user/init.c
@@ -51,10 +51,10 @@ int main() {
   puti(p);
   kputc('\n');
   int64_t lt = 0;
-  // fork();
+  fork();
   while(1){
     int64_t t = uptime();
-    if (t - lt >= 1000) {
+    if (t - lt >= 100) {
       puti64(t);
       lt = t;
       kputc(' ');

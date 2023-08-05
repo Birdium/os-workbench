044fa7ce2a0deb6855416bdddf0ba245a6883eaf
tracer-nju <tracer@nju.edu.cn>
2023-08-05 14:23:22 +0800

    Linux Birdium-Laptop 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  14:23:22 up  1:23,  1 user,  load average: 0.11, 0.06, 0.01

--- .shadow/abstract-machine/am/src/x86/qemu/vme.c
+++ .shadow/abstract-machine/am/src/x86/qemu/vme.c
@@ -91,7 +91,7 @@ bool vme_init(void *(*_pgalloc)(int size), void (*_pgfree)(void *)) {
   panic_on(cpu_current() != 0, "init VME in non-bootstrap CPU");
   pgalloc = _pgalloc;
   pgfree  = _pgfree;
-  printf("uvm_area : [%p, %p)\n", vm_areas[0].area.start, vm_areas[0].area.end);
+  printf("uvm_area : [%p, %p)\n", vm_areas[1].area.start, vm_areas[1].area.end);
 
 #if __x86_64__
   kpt = (void *)PML4_ADDR;

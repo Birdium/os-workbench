ea8c427194620fadbf15b3ee239bad7f2b589bea
tracer-nju <tracer@nju.edu.cn>
2023-08-06 05:41:47 +0800

    Linux Birdium-Laptop 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  05:41:47 up 16:41,  1 user,  load average: 0.35, 0.20, 0.14

--- .shadow/kernel/src/kmt/kmt.c
+++ .shadow/kernel/src/kmt/kmt.c
@@ -44,9 +44,9 @@ static inline task_t *poll_rand_task() {
     //     cnt = 0;
     // }
     // cnt++;
-    if (task_list[0]->running == 1 && task_list[1]->running == 1) {
-        printf("[11 %p]\n", cur_task);
-    }
+    // if (task_list[0]->running == 1 && task_list[1]->running == 1) {
+    //     printf("[11 %p]\n", cur_task);
+    // }
     static const int round = 2; // choose task_cnt times for X round
     for (int i = 0; i < task_cnt * round; i++) {
         int idx = rand() % task_cnt;

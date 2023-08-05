8a6513639c550cf88b8487648e34dd47132b9068
tracer-nju <tracer@nju.edu.cn>
2023-08-06 05:23:51 +0800

    Linux Birdium-Laptop 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  05:23:51 up 16:24,  1 user,  load average: 0.01, 0.06, 0.16

--- .shadow/user/init.c
+++ .shadow/user/init.c
@@ -33,28 +33,28 @@ void puti64(int64_t x) {
 
 int main() {
   // Example:
-  kputc('H');
-  kputc('e');
-  kputc('l');
-  kputc('l');
-  kputc('o');
-  kputc(',');
-  kputc(' ');
-  kputc('w');
-  kputc('o');
-  kputc('r');
-  kputc('l');
-  kputc('d');
-  kputc('!');
-  kputc('\n');
-  int64_t lt = 0;
-  int fk = fork();
-  kputc('0' + fk);
-  kputc('\n');
-  int p = getpid();
-  // int fk2 = fork();
-  // kputc('0' + fk2);
+  // kputc('H');
+  // kputc('e');
+  // kputc('l');
+  // kputc('l');
+  // kputc('o');
+  // kputc(',');
+  // kputc(' ');
+  // kputc('w');
+  // kputc('o');
+  // kputc('r');
+  // kputc('l');
+  // kputc('d');
+  // kputc('!');
   // kputc('\n');
+  // int fk = fork();
+  // kputc('0' + fk);
+  // kputc('\n');
+  int64_t lt = 0;
+  int p = getpid();
+  int fk2 = fork();
+  kputc('0' + fk2);
+  kputc('\n');
   while(1){
     int64_t t = uptime();
     if (t - lt >= 100) {

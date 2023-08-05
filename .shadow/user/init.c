0d3a1389c3682c2adf372e1b3f905f8c588c83b2
tracer-nju <tracer@nju.edu.cn>
2023-08-05 21:08:39 +0800

    Linux Birdium-Laptop 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:08:39 up  8:08,  1 user,  load average: 0.28, 0.20, 0.10

--- .shadow/user/init.c
+++ .shadow/user/init.c
@@ -18,15 +18,19 @@ int main() {
   kputc('!');
   kputc('\n');
   int p = getpid();
-  char buf[10];
-  memset(buf, 0, sizeof(buf));
-  int m = 1;
-  for (int q = p; q != 0; q /= 10) {
-    m *= 10;
+  while (p) {
+    kputc('0' + p % 10);
+    p /= 10;
   }
-  for (int n = m; n != 0; n /= 10, p /= 10) {
-    kputc('0' + p / (n / 10));
-  }
+  // char buf[10];
+  // memset(buf, 0, sizeof(buf));
+  // int m = 1;
+  // for (int q = p; q != 0; q /= 10) {
+  //   m *= 10;
+  // }
+  // for (int n = m; n != 0; n /= 10, p /= 10) {
+  //   kputc('0' + p / n);
+  // }
   while(1){
     uptime();
   }

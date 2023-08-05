a0b677192b93607efdd948254c68dfbc7c1a214d
tracer-nju <tracer@nju.edu.cn>
2023-08-06 06:17:02 +0800

    Linux Birdium-Laptop 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  06:17:02 up 17:17,  1 user,  load average: 0.31, 0.26, 0.16

--- .shadow/user/init.c
+++ .shadow/user/init.c
@@ -58,10 +58,10 @@ int main() {
     if (t - lt >= 100) {
       puti(p);
       lt = t;
-      // if (fk && t / 100 == 10) {
-      //   kill(fk);
-      // }
-      // kputc(' ');
+      if (fk && t / 100 == 10) {
+        kill(fk);
+      }
+      kputc(' ');
     }
   }
   return p;

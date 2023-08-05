b9ca164c5d37612b5eba8cfc9f29fbf502073a31
tracer-nju <tracer@nju.edu.cn>
2023-08-06 06:22:13 +0800

    Linux Birdium-Laptop 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  06:22:13 up 17:22,  1 user,  load average: 0.10, 0.23, 0.18

--- .shadow/kernel/src/kmt/kmt.c
+++ .shadow/kernel/src/kmt/kmt.c
@@ -51,7 +51,7 @@ static inline task_t *poll_rand_task() {
     for (int i = 0; i < task_cnt * round; i++) {
         int idx = rand() % task_cnt;
         task_t *task = task_list[idx];
-        if (task->status != SLEEPING && (atomic_xchg(&task->running, 1) == 0)) { // atomic set task running 
+        if (task->status != SLEEPING && (task == cur_task || atomic_xchg(&task->running, 1) == 0)) { // atomic set task running 
             result = task;
             break;
         }

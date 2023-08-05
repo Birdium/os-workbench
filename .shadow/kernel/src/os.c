021a1ee655705bb408b74e2be57b8b6a79472d4c
tracer-nju <tracer@nju.edu.cn>
2023-08-06 03:51:24 +0800

    Linux Birdium-Laptop 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  03:51:24 up 14:51,  1 user,  load average: 0.08, 0.20, 0.22

--- .shadow/kernel/src/os.c
+++ .shadow/kernel/src/os.c
@@ -124,7 +124,7 @@ Context *os_trap(Event ev, Context *context) {
     panic_on(!next, "returning NULL context");
   }
   panic_on(!sane_context(next), "returning to invalid context");
-  // LOG_USER("trap returning %p with rip %x", next, next->rip);
+  LOG_USER("trap returning %p with rip %x", next, next->rip);
   TRACE_EXIT;
   return next;
 }

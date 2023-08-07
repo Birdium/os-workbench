5a5a6afe0f071b413da08708d85f699e6c1011b8
tracer-nju <tracer@nju.edu.cn>
2023-08-08 00:08:09 +0800

    Linux Birdium-Laptop 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  00:08:09 up  1:33,  1 user,  load average: 0.49, 0.17, 0.05

--- .shadow/kernel/src/uproc.c
+++ .shadow/kernel/src/uproc.c
@@ -245,8 +245,8 @@ int uproc_exit(task_t *task, int status) {
 	}
 	pinfo[pid].mappings->free(pinfo[pid].mappings);
 	// FIXME: orphan proc
+	printf("%d\n", 111);
 	if (pinfo[task->ppid].valid) {
-		printf("%d\n", 111);
 		task_t *father = pinfo[task->ppid].task;
 		father->child_cnt--;
 		if (father->waiting) {

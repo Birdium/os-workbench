96e4bc39d27dde594450b34f9f4caedb6e8ec94c
tracer-nju <tracer@nju.edu.cn>
2023-08-08 22:21:51 +0800

    Linux Birdium-Laptop 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  22:21:51 up  2:55,  1 user,  load average: 0.10, 0.13, 0.09

--- .shadow/kernel/include/uproc.h
+++ .shadow/kernel/include/uproc.h
@@ -4,16 +4,7 @@
 #include "os.h"
 #include "list.h"
 
-#define MAP_SHARED    1
-#define MAP_PRIVATE   2
-#define MAP_UNMAP     3
-
-#define MMAP_NONE  0x00000000 // no access
-#define MMAP_READ  0x00000001 // can read
-#define MMAP_WRITE 0x00000002 // can write
-
 #define UPROC_PID_NUM 32768
-
 
 typedef struct mapping {
 	void *va, *pa;

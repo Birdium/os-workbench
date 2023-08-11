2052387e0dd81c14661260742640097cec8c3974
tracer-nju <tracer@nju.edu.cn>
2023-08-11 19:13:46 +0800

    Linux Birdium-Laptop 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  19:13:46 up 38 min,  1 user,  load average: 0.33, 0.38, 0.18

--- .shadow/kernel/src/uproc/uproc.c
+++ .shadow/kernel/src/uproc/uproc.c
@@ -149,7 +149,7 @@ static Context *pagefault_handler(Event ev, Context *context) {
   void *va = (void *)(ev.ref & pg_mask);
   LOG_USER("task: %s, %s, %d", cur_task->name, ev.msg, ev.cause);
 //   LOG_USER("%p %p %p(%p)", as, pa, va, ev.ref);
-  pgnewmap(cur_task, va, pa, PROT_READ | PROT_WRITE, MAP_PRIVATE);
+  pgnewmap(cur_task, va, pa, PROT_READ, MAP_PRIVATE);
   return NULL;
 }
 
